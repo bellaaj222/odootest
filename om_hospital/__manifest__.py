4085fa7cb198ac049630790539f9bda2826d8943
Houssem Bellaaj <you@example.com>
2023-05-11 14:54:29 +0100

    from home

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Odoo HTA',
+    'sequence': -100,
+    'summary': 'Hospital managment system',
+    'description': """Hospital management system""",
+
+    'depends': [],
+    'data': ['security/ir.model.access.csv',
+             'views/menu.xml',
+             'views/patient_view.xml'
+             ],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
