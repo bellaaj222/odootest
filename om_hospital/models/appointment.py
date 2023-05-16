4f7b37a8b9220e0228b958b1b42c5dd69605654b
houssem <bellaaj222@gmail.com>
2023-05-16 13:28:34 +0100

    test 2

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -5,9 +5,15 @@ class HospitalAppointement(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
-
+    _rec_name = 'patient_id'
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
-    gender = fields.Selection(related='patient_id.gender') #readonly=False pour editer
+    gender = fields.Selection(related='patient_id.gender')  # readonly=False pour editer
 
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
+    ref = fields.Char(string="Reference")
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        print(self, self.patient_id, self.patient_id.ref)
+        self.ref = self.patient_id.ref  # POUR ENTRER LE REF QUI EXISTER DANS PATIENT
