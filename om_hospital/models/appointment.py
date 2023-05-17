099e0f960ede91c769b4fed597e0a960dc6243b2
houssem <bellaaj222@gmail.com>
2023-05-17 14:18:03 +0100

    test 4 17

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -11,12 +11,24 @@ class HospitalAppointement(models.Model):
 
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
-    ref = fields.Char(string="Reference")
+    ref = fields.Char(string="Reference", help="Reference of the patient")
     prescription = fields.Html(string="Prescription")
     priority = fields.Selection([('0', 'Very Low'), ('1', 'Low'), ('2', 'Normal'), ('3', 'High')],
-                                string='Priority') #pour les starts de priotirity
+                                string='Priority')  # pour les starts de priotirity
+    state = fields.Selection(
+        [('draft', 'Draft'), ('in_consultation', 'In_consultation'), ('done', 'Done'), ('cancel', 'Cancel')],
+        string='Status', default="draft", required="True")  # pour les starts de priotirity
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         print(self, self.patient_id, self.patient_id.ref)
         self.ref = self.patient_id.ref  # POUR ENTRER LE REF QUI EXISTER DANS PATIENT
+
+    def action_test(self):
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Click Successfully',
+                'type': 'rainbow_man',
+            }
+        }
