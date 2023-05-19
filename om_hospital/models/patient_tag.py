33f1851e35f3188a6231c531b10675afb6147b2d
houssem <bellaaj222@gmail.com>
2023-05-19 14:37:16 +0100

    test 4 19

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -1,9 +1,10 @@
 from odoo import api, fields, models
 
 
-class PatientTAg(models.Model):
+class PatientTag(models.Model):
     _name = "patient.tag"
     _description = "Patient Tag"
 
     name = fields.Char(string="Name", required=True)
     active = fields.Boolean(string="Active", default=True)
+    color = fields.Integer(string="Color")
