fde3513593bd0c89de00bc58c35dec6047f90128
houssem <bellaaj222@gmail.com>
2023-06-06 12:15:51 +0100

    test  06/06

---
+++ eb_flow_wizard/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Test Management',
+    'version': '1.0.0',
+    'category': 'test',
+    'author': 'Odoo HTA',
+    'sequence': -100,
+    'summary': 'Test managment system',
+    'description': """TESt management system""",
+
+    'depends': ['base', 'hr', 'product'],
+    'data': [],
+    'demo': [],
+    'auto_install': False,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
