6f6f9b01c6946fb8e0d5ef7ebdfff447e46df026
houssem <bellaaj222@gmail.com>
2023-08-31 12:09:39 +0100

    version diagram odoo 11 vers odoo 15 final stop

---
+++ generic_m2o/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Generic M2O Widget",
+    "version": "15.0.1.5.0",
+    "author": "Center of Research and Development",
+    "website": "https://crnd.pro",
+    "license": "LGPL-3",
+    "summary": "Generic Many2one widget",
+    'category': 'Web',
+    'depends': [
+        'web',
+    ],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            '/generic_m2o/static/src/js/generic_m2o.js',
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
